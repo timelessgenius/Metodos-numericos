8105d9cfc728a22ce8b30a51142ed55d77427896
Matheus Costa <mcss.pb1@gmail.com>
2026-09-12 15:59:47 -0300

    Implementação do algoritmo pra resolver sistemas lineares triangulares superiores

---
+++ treino/UpperTriangularSystem.py
@@ -0,0 +1,24 @@
+from det import solveDeterminante
+
+U = [[5,-2,6,1],[0,3,7,-4],[0,0,4,5],[0,0,0,2]]
+b = [1,-2,28,8]
+
+def calculateUpperTriangularSystem(matriz,vetor):
+    tamanho = len(U)
+    ans = len(U)*[0]
+    for i in range(tamanho-1,-1,-1): # O range tem a sintaxe : (inicio, fim, passo), como um vetor ou lista funciona de 0 até tamanho-1, vamos colocar que ele começa de tamanho-1, que ele vai até o indice -1 (incluir o zero) e que ele vai funcionar na onrdem inversa (decremnto)
+        soma = 0
+        for j in range(i+1,tamanho):
+            soma += U[i][j]*ans[j]
+        ans[i] = (b[i]- soma)/U[i][i]
+    return ans
+
+det = solveDeterminante(U,len(U))
+
+if det != 0:
+    print("O determinante dessa matriz é:", det)
+    print(calculateUpperTriangularSystem(U,b))
+else:
+    print("Essa matriz não possui solução única")
+
+
